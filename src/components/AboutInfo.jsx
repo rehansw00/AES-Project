8facaabc36def7a56d1d51a5a30a154b942a456a
Sayyed Rehan <rehansw00@gmail.com>
2025-02-08 22:10:02 +0530

    new

--- src/components/AboutInfo.jsx
+++ src/components/AboutInfo.jsx
@@ -4,7 +4,7 @@ const AboutInfo = () => {
   return (
     <div className="about-info">
         <div className="vc-office">
-            <h1>Office of the <br /> Vice Chancellor</h1>
+            <h1>Office of the <br /> Principal</h1>
             <p>At school, we are driven by a relentless pursuit of academic excellence and a commitment to fostering a vibrant learning community. Our institution stands as a beacon of knowledge, providing a transformative educational experience that empowers individuals to lead, innovate, and make a positive impact on the world. In our pursuit of excellence, we continuously evolve our curriculum, embrace new technologies, and promote interdisciplinary collaboration.</p>
         </div>
         <div className="vc-image">
